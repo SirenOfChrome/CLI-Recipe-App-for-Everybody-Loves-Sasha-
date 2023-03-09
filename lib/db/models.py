3eea2a784cea7a146c01a936f50f42269e2d2f94
Madison Evans <m.corbinevans@gmail.com>
2023-03-09 07:22:37 -0700

    updated models.py and tested db

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,15 +1,11 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-
-Base = declarative_base()
-
-
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
+from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+engine = create_engine("sqlite:///cookbook.db")
+Session = sessionmaker(bind=engine)
 Base = declarative_base()
+
 
 class Recipe(Base):
     __tablename__ = 'recipe'
@@ -23,43 +19,55 @@ class Recipe(Base):
 
     user = relationship('User', backref='recipes')
     instructions = relationship('Instructions', backref='recipes')
-    
+    ingredients = relationship("Recipe_Ingredient", back_populates="recipe")
+
     def __repr__(self):
         return f'<Recipe {self.recipe_name}>'
+
 
 class User(Base):
     __tablename__ = 'user'
 
     user_id = Column(Integer, primary_key=True)
-    user_name = Column(String)
-    
+    first_name = Column(String)
+    last_name = Column(String)
+
     def __repr__(self):
         return f'<User {self.user_name}>'
+
 
 class Instructions(Base):
     __tablename__ = 'instructions'
 
     instruction_id = Column(Integer, primary_key=True)
     instruction_details = Column(String)
-    
+
     def __repr__(self):
         return f'<Instructions {self.instruction_id}>'
 
-class Recipe_Ingredients(Base):
-    __tablename__ = 'recipe_ingredients'
-    recipe_id = Column(Integer, ForeignKey('recipes.id'), primary_key=True)
-    ingredient_id = Column(Integer, ForeignKey('ingredients.id'), primary_key=True)
+
+class Recipe_Ingredient(Base):
+    __tablename__ = 'recipe_ingredient'
+
+    recipe_id = Column(Integer, ForeignKey('recipe.recipe_id'), primary_key=True)
+    ingredient_id = Column(Integer, ForeignKey('ingredient.id'), primary_key=True)
     quantity = Column(Integer)
-    
+
     recipe = relationship("Recipe", back_populates="ingredients")
     ingredient = relationship("Ingredient", back_populates="recipes")
 
+
 class Ingredient(Base):
-    __tablename__ = 'ingredients'
+    __tablename__ = 'ingredient'
+
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    
-    recipes = relationship("Recipe_Ingredients", back_populates="ingredient")
 
+    recipes = relationship("Recipe_Ingredient", back_populates="ingredient")
 
-
+Base.metadata.create_all(engine)
+session = Session()
+# Get the recipe you want to delete
+madison = session.query(User).filter_by(first_name='Madison').first()
+session.delete(madison)
+session.commit()
