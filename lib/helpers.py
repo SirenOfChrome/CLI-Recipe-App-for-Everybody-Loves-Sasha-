feea8e17ca23bef3b83f7877e32d3f0376ce9387
Madison Evans <m.corbinevans@gmail.com>
2023-03-09 22:57:53 -0700

    made some presentation updates on command line

--- lib/helpers.py
+++ lib/helpers.py
@@ -14,16 +14,20 @@ def add_user():
     name_tuple = ()
     
     # prompt user to enter first name and add it to name_tuple
-    print("Enter your first name:")
+
+    print("\n*** Enter your first name:\n")
     first_name = input()
+    print("\n")
     if(first_name == "clear entire db"): 
         clearDatabase()
         return
     name_tuple += (first_name,)
     
     # prompt user to enter last name and add it to name_tuple
-    print("Enter your last name:")
+    print("*** Enter your last name:")
+    print("\n")
     last_name = input()
+    print("\n")
     name_tuple += (last_name,)
     
     # check if user exists in the database using the user_exists function
@@ -35,8 +39,10 @@ def add_user():
         session.commit()
         # print success message and add a recipe for the new user
         print(f"User {first_name} {last_name} successfully added!")
+        print("\n")
     else:
         print(f"Welcome back {first_name} {last_name}!")
+        print("\n")
         
     recipe = add_recipe(current_user)
     session.add(recipe)
@@ -51,20 +57,26 @@ def user_exists(session, first_name, last_name):
 # function to add a recipe for a given user
 def add_recipe(user): 
     # prompt user to enter the recipe name
-    print("Enter the recipe name: ")
+    print("*** Enter the recipe name: ")
+    print("\n")
     recipe_name = input()
+    print("\n")
     
     # prompt user to enter the total cook time for the recipe (prep time included)
-    print("How much time is needed to cook this meal? (prep time included): ")
+    print("*** How much time is needed to cook this meal? (Write your answer in minutes, prep time included): ")
+    print("\n")
     total_cook_time = input()
+    print("\n")
     
     # prompt user to enter the instructions for the recipe
-    print("Enter each of the instructions, separated by ';' for each step")
+    print("*** Enter each of the instructions, separated by ';' for each step")
+    print("\n")
     instructions = input()
+    print("\n")
     
     # create a new Recipe object and add it to the database
     recipe = Recipe(recipe_name=recipe_name, total_cook_time=total_cook_time, user_id=user.user_id, instructions=instructions)
-    print("Recipe added")
+    print("Recipe added! \n")
     return recipe
 
 # function to build a dictionary of ingredients and their quantities
@@ -73,8 +85,9 @@ def build_ingredient_dictionary():
     ingredient_dict = {}
     
     # prompt user to enter ingredients and quantities in the format 'ingredient:quantity', separated by a colon
-    print("Enter an ingredient (singular-case) along with its quantity, separated by a colon (ex: egg:3). When you're finished, type DONE and hit enter.")
+    print("Enter an ingredient (singular-case) along with its quantity, separated by a colon (ex: egg:3). When you're finished, type DONE and hit enter.\n")
     response = input()
+    print("\n")
     while response != "DONE":
         try:
             # split user input to extract ingredient and quantity, convert quantity to integer, and add it to the ingredient_dict
@@ -85,6 +98,7 @@ def build_ingredient_dictionary():
             print(f"Invalid quantity '{quantity}'. Please enter a valid integer.")
         print("Waiting for next entry... (type DONE if finished)") 
         response = input()
+        print("\n")
     print(ingredient_dict)
     return ingredient_dict
 
